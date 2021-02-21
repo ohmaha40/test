92aa79f6e334c079587abd10938fd5a7109f503f
Ohmaha40 <keller.chris@outlook.de>
2021-02-21 18:10:43 +0100

    test

--- Funktionen/Die return-Anweisung/main.js
+++ Funktionen/Die return-Anweisung/main.js
@@ -7,5 +7,9 @@ const multiplizieren = function (a, b) {
 let z_1 = 13;
 let z_2 = 111;
 
-let ergebnis = multiplizieren(z_1, z_2);
-console.log(ergebnis);
+//let ergebnis = multiplizieren(z_1, z_2);
+console.log(multiplizieren(z_1, z_2));
+
+//let ergebnis_2 = multiplizieren(multiplizieren(z_1, z_2), multiplizieren(z_1, z_2));
+console.log(multiplizieren(multiplizieren(z_1, z_2), multiplizieren(z_1, z_2)));
+
