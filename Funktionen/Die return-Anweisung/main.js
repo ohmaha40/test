e81767b246d89775b7381cc1ff6e211214d6532a
Ohmaha40 <keller.chris@outlook.de>
2021-02-21 18:01:08 +0100

    pull

---
+++ Funktionen/Die return-Anweisung/main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const multiplizieren = function (a, b) {
+    return a * b;
+};
+
+let z_1 = 13;
+let z_2 = 111;
+
+let ergebnis = multiplizieren(z_1, z_2);
+console.log(ergebnis);
