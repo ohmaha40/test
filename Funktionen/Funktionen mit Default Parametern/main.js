3f84fd67f87a4b4e6abf6a02195e1c3cd5dfd6fd
Ohmaha <keller.chris@outlook.de>
2021-02-21 11:48:44 +0100

    3ter

--- Funktionen/Funktionen mit Default Parametern/main.js
+++ Funktionen/Funktionen mit Default Parametern/main.js
@@ -1,1 +1,12 @@
 "use strict";
+
+const multiplitieren = function (a, b = 10) {
+    console.log(a);
+    console.log(b);
+    console.log(a * b);
+};
+
+let z_1 = 42;
+let z_2 = 18;
+
+multiplitieren(z_1);
