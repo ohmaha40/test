6c62dcab3f8787bf18f495353ec1602f1bcdc524
Ohmaha <keller.chris@outlook.de>
2021-02-21 12:01:56 +0100

    test

--- Funktionen/Funktionen mit Default Parametern/main.js
+++ Funktionen/Funktionen mit Default Parametern/main.js
@@ -1,12 +1,27 @@
 "use strict";
 
-const multiplitieren = function (a, b = 10) {
+const multiplitieren = function (a = 20, b = 10) {
     console.log(a);
     console.log(b);
     console.log(a * b);
 };
 
 let z_1 = 42;
-let z_2 = 18;
+let z_2 = 20;
 
-multiplitieren(z_1);
+multiplitieren(z_1, z_2);
+
+
+// funktion 3 para  vor nach alter  cons hallo etc    default werder 10 max mustermann   3 var 2 mal aufrufen mit und ohne
+
+
+const begruessung = function (vor = "Max", nach = "Mustermann", alter = 18) {
+    console.log(`Hallo ${vor} ${nach}, du bist ${alter} Jahre alt!`);
+};
+
+let vorname = "Christian";
+let nachname = "Keller";
+let alterv = 40;
+
+begruessung(vorname,nachname,alterv);
+begruessung();
