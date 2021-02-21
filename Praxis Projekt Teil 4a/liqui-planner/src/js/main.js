226613349b3abf39f4110f04f38f406121c81b8e
Ohmaha40 <keller.chris@outlook.de>
2021-02-21 21:07:02 +0100

    praxis 4

--- Praxis Projekt Teil 4a/liqui-planner/src/js/main.js
+++ Praxis Projekt Teil 4a/liqui-planner/src/js/main.js
@@ -6,74 +6,62 @@
 *           Keller Christian
 *
 *
-*           !! promt eingabe sind Strings
-*           !! parseInt convertiert in Int
-*
-*           AUfgabe: else if
-*
+*           
 */
 // Titel Eingabe
 
-let bilanz_einnahmen = 0;
-let bilanz_ausgaben = 0;
-let bilanz_summe = 0;
+let bilanz_einnahmen = 0,
+    bilanz_ausgaben = 0,
+    bilanz_summe = 0,
+    titel,
+    typ,
+    betrag,
+    datum;
 
-let titel_1 = prompt("Titel: ", "Einkaufen");
-let typ_1 = prompt("Typ: ", "Einnahme oder Ausgabe");
-let betrag_1 = parseInt(prompt("Betrag: ", "in Cent"));
-let datum_1 = prompt("Datum: ", "jjjj-mm-tt");
+const eintrag_erfassen = function () {
+    titel = prompt("Titel: ", "Einkaufen");
+    typ = prompt("Typ: ", "Einnahme oder Ausgabe");
+    betrag = parseInt(prompt("Betrag: ", "in Cent"));
+    datum = prompt("Datum: ", "jjjj-mm-tt");
+};
 
-if (typ_1 === "Einnahme") { //Muss String sein
-    bilanz_einnahmen = bilanz_einnahmen + betrag_1;
-    bilanz_summe = bilanz_summe + betrag_1;
-} else if ( typ_1 === "Ausgabe") {
-    bilanz_ausgaben = bilanz_ausgaben + betrag_1;
-    bilanz_summe = bilanz_summe - betrag_1;
-} else {
-    console.log(`Der Typ "${typ_1}" ist nicht bekannt`);
+const eintag_ausgeben = function (a, b, c, d) {
+    console.log(`
+Titel: ${a}
+Typ: ${b}
+Betrag: ${c} Cent
+Datum: ${d}
+`);
 }
 
-console.log(`
-Titel: ${titel_1}
-Datum: ${datum_1}
-Typ: ${typ_1}
-Betrag: ${betrag_1} Cent
-Einnahmen: ${bilanz_einnahmen} Cent
-Ausgaben: ${bilanz_ausgaben} Cent
-Bilanz: ${bilanz_summe} Cent
+const eintrag_mit_gesamtbilanz_verechnen = function (typ, betrag) {
+    if (typ === "Einnahme") {
+        bilanz_einnahmen = bilanz_einnahmen + betrag;
+        bilanz_summe = bilanz_summe + betrag;
+    } else if (typ === "Ausgabe") {
+        bilanz_ausgaben = bilanz_ausgaben + betrag;
+        bilanz_summe = bilanz_summe - betrag;
+    } else {
+        console.log(`Der Typ "${typ}" ist nicht bekannt`);
+    }
+};
+
+const gesamtbilanz_ausgeben = function (gesamteinnahmen, gesamtausgaben, summe) {
+    console.log(`
+Einnahmen: ${gesamteinnahmen} Cent
+Ausgaben: ${gesamtausgaben} Cent
+Bilanz: ${summe} Cent
+Bilianz ist positiv: ${summe >= 0}
 `);
+};
 
-// Titel 2 Eingabe
-let titel_2 = prompt("Titel: ", "Einkaufen");
-let typ_2 = prompt("Typ: ", "Einnahme oder Ausgabe");
-let betrag_2 = parseInt(prompt("Betrag:", "in Cent"));
-let datum_2 = prompt("Datum: ", "jjjj-mm-tt");
+const eintrag_hinzufuegen = function () {
+    eintrag_erfassen();
+    eintag_ausgeben(titel, typ, betrag, datum);
+    eintrag_mit_gesamtbilanz_verechnen(typ, betrag);
+    gesamtbilanz_ausgeben(bilanz_einnahmen, bilanz_ausgaben, bilanz_summe);
+};
 
-if (typ_2 === "Einnahme") {
-    bilanz_einnahmen = bilanz_einnahmen + betrag_2;
-    bilanz_summe = bilanz_summe + betrag_2;
-} else if ( typ_2 === "Ausgabe") {
-    bilanz_ausgaben = bilanz_ausgaben + betrag_2;
-    bilanz_summe = bilanz_summe - betrag_2;
-} else {
-    console.log(`Der Typ "${typ_2}" ist nicht bekannt`);
-}
-
-console.log(`
-Titel: ${titel_2}
-Datum: ${datum_2}
-Typ: ${typ_2}
-Betrag: ${betrag_2} Cent
-Einnahmen: ${bilanz_einnahmen} Cent
-Ausgaben: ${bilanz_ausgaben} Cent
-Bilanz: ${bilanz_summe} Cent
-`);
-
-//Variable für Bilanz anlegen
-let positiv = bilanz_summe >= 0;
-console.log(`
-Einnahmen: ${bilanz_einnahmen} Cent
-Ausgaben: ${bilanz_ausgaben} Cent
-Bilanz: ${bilanz_summe} Cent
-Bilianz ist positiv: ${positiv}
-`);
+eintrag_hinzufuegen();
+eintrag_hinzufuegen();
+eintrag_hinzufuegen();
