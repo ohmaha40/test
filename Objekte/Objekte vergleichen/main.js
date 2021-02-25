87c9cec2de2f66ecbd8369db3d690a1ba8050de8
Ohmaha40 <keller.chris@outlook.de>
2021-02-25 21:29:43 +0100

    praxis 5 und switch anfang

--- Objekte/Objekte vergleichen/main.js
+++ Objekte/Objekte vergleichen/main.js
@@ -19,3 +19,17 @@ console.log(auto_1 == auto_2);
 console.log(auto_2 == auto_3);
 console.log(auto_3 == auto_3);
 console.log({} == {});
+
+//challenge: schreibe eine Funktion, die zwei Auto-Obkejte mit dem Eigeschaften "marke" und "modell"
+//übergeben bekommt und prüft, ob die Objekte gleich sind.
+
+// const objekte_vergleichen = function (ob_a,ob_b) {
+//     if (ob_a.marke == ob_b.marke && ob_a.modell == ob_b.modell ) {
+//       return true;
+//     } else {
+//     return false;
+//     }   
+// };
+
+const vergleichen = function (ob_a, ob_b) { return ob_a.marke == ob_b.marke && ob_a.modell ==ob_b.modell;}
+console.log(vergleichen(auto_1, auto_2));
