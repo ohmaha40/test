43ff1b8c8aa00a37a4afc63523f931f753e5e15d
Ohmaha40 <keller.chris@outlook.de>
2021-02-26 21:37:05 +0100

    arrays

---
+++ Arrays/Mehrdimensionale Arrays/main.js
@@ -0,0 +1,60 @@
+"use strict";
+
+// let obst = [
+//     "Äpfel",
+//     "Birnen",
+//     "Bananen"
+// ];
+
+// let gemuese = [
+//     "Möhren",
+//     "Sellerie",
+//     "Kohl"
+// ];
+
+// let brot = [
+//     "Graubrot",
+//     "Schwarzbrot",
+//     "Vollkornbrot"
+// ];
+
+// let waren = [
+//     obst,
+//     gemuese,
+//     brot
+// ];
+
+let waren = [
+    [
+        "Äpfel",
+        "Birnen",
+        "Bananen"
+    ],
+    [
+        "Möhren",
+        "Sellerie",
+        "Kohl"
+    ],
+    [
+        "Graubrot",
+        "Schwarzbrot",
+        "Vollkornbrot"
+    ]
+];
+
+console.log(waren);
+console.log(waren[0]);
+console.log(waren[1]);
+console.log(waren[2]);
+
+console.log(waren[0][0]);
+console.log(waren[0][1]);
+console.log(waren[0][2]);
+
+console.log(waren[1][0]);
+console.log(waren[1][1]);
+console.log(waren[1][2]);
+
+console.log(waren[2][0]);
+console.log(waren[2][1]);
+console.log(waren[2][2]);
