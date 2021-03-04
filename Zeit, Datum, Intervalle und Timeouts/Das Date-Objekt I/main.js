f05986b031bb625ef768d2ab453d486c2ad25157
Ohmaha40 <keller.chris@outlook.de>
2021-03-04 21:29:46 +0100

    datum und zeit

--- Zeit, Datum, Intervalle und Timeouts/Das Date-Objekt I/main.js
+++ Zeit, Datum, Intervalle und Timeouts/Das Date-Objekt I/main.js
@@ -1,7 +1,16 @@
 "use strict";
 
 let d = new Date(); //aktuelles datum
-console.log(d);
 
+console.log(d);
 console.log(Date.now());  //UNIX Zeit seit 1.1.1970 um 00:00
 
+let e = new Date(1414887762346);
+console.log(e);
+
+let f = new Date("12 24 2014");  // muss richtige form haben  iso 8601
+console.log(f);
+
+// new Date(jahr, monat-index[, tag[, stunde[,minute[,sekunde[,millisekunde]]]]])
+let g = new Date(2012, 5/*index monat-1*/, 23, 15, 15, 15, 155);
+console.log(g);
---
+++ Zeit, Datum, Intervalle und Timeouts/Das Date-Objekt II/main.js
@@ -0,0 +1,53 @@
+"use strict";
+
+let d = new Date();
+
+// setzen
+d.setFullYear(1993);
+d.setMonth(/*index*/0);
+d.setDate(16);
+d.setHours(9);
+d.setMinutes(32);
+d.setSeconds(12);
+d.setMilliseconds(111);
+
+console.log(d.getFullYear());
+console.log(d.getMonth());
+console.log(d.getDate());
+console.log(d.getDay());
+console.log(d.getHours());
+console.log(d.getMinutes());
+console.log(d.getSeconds());
+console.log(d.getMilliseconds());
+
+
+
+console.log(d);
+
+
+let d_utc = new Date();
+
+// setzen
+d_utc.setUTCFullYear(1993);
+d_utc.setUTCMonth(/*index*/0);
+d_utc.setUTCDate(16);
+d_utc.setUTCHours(9);
+d_utc.setUTCMinutes(32);
+d_utc.setUTCSeconds(12);
+d_utc.setUTCMilliseconds(111);
+
+console.log(d_utc);
+
+console.log(d.getUTCFullYear());
+console.log(d.getUTCMonth());
+console.log(d.getUTCDate());
+console.log(d.getUTCDay());
+console.log(d.getUTCHours());
+console.log(d.getUTCMinutes());
+console.log(d.getUTCSeconds());
+console.log(d.getUTCMilliseconds());
+
+let d_unix = new Date();
+d_unix.setTime(1614889640000);
+console.log(d_unix);
+console.log(d_unix.getTime());
